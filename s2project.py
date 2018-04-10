6384ed9afa893cc95e3f538d64a9ea1faadd9108
Chris <releasethebadgers@yahoo.co.uk>
2018-04-10 20:26:29 +0100

    Connect to mongoDB

--- s2project.py
+++ s2project.py
@@ -2,13 +2,19 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'Adoptdata'
-COLLECTION_NAME = 'projects'
+# when hosting via local server
+# MONGODB_HOST = 'localhost'
+# MONGODB_PORT = 27017
+# DBS_NAME = 'Adoptdata'
+
+# when hosting via heroku
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'Adoptdata')
+COLLECTION_NAME = 'adoptionData'
 
 
 @app.route("/")
@@ -34,7 +40,7 @@ def Adoptdata():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
